60136c497e1d0a6a9368b2110fcbac1c73d612ea
CarlosRuiz <ingecarlosrm@gmail.com>
2021-03-08 11:45:46 -0600

    [UPDATE] main grid sections

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function navSlide() {
     const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+    const nav = document.querySelector(".nav-list");
+    const navLinks = document.querySelectorAll(".nav-list li");
     
     burger.addEventListener("click", () => {
         //Toggle Nav
