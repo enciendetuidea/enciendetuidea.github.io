2eedaf2ccbbae1f56996d1e5586ebfc96974529e
CarlosRuiz <ingecarlosrm@gmail.com>
2021-03-09 23:32:25 -0600

    [ADD] Countdown

--- main.js
+++ main.js
@@ -28,3 +28,32 @@ window.addEventListener('scroll', function () {
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
 })
+
+
+var countDate = new Date('Apr 1, 2021 00:00:00').getTime();
+
+function newYear(){
+    var now = new Date().getTime();
+        gap = countDate - now;
+
+        var second = 1000;
+        var minute = second * 60;
+        var hour = minute * 60;
+        var day = hour * 24;
+
+        var d = Math.floor(gap / (day));
+        var h = Math.floor((gap % (day)) / (hour));
+        var m = Math.floor((gap % (hour)) / (minute));
+        var s = Math.floor((gap % (minute)) / (second));
+
+        document.getElementById('day').innerText = d;
+        document.getElementById('hour').innerText = h;
+        document.getElementById('minute').innerText = m;
+        document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    newYear();
+},1000)
+
+
